36fca4453260afd293f691c00d93e642c4151e02
DziBab <3169477@gmail.com>
2024-10-18 00:01:42 +0200

    add flake8 lint to ci

--- app/main.py
+++ app/main.py
@@ -1,16 +1,19 @@
 from fastapi import FastAPI
 
-
 from .db.session import Base, engine
 from .routes import spending
 
+
 app = FastAPI()
+
 
 # Create the database tables
 Base.metadata.create_all(bind=engine)
 
+
 # Include routers
 app.include_router(spending.router)
+
 
 @app.get("/")
 def read_root():
