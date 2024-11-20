0159c4f0f142584e6a9f81e8d32f74c93c6298e1
DziBab <3169477@gmail.com>
2024-11-20 22:11:00 +0100

    add enums with default categories and currencies

---
+++ src/enums.py
@@ -0,0 +1,21 @@
+from enum import StrEnum
+
+# Default currencies enum
+class Currency(StrEnum):
+    USD = "USD"
+    EUR = "EUR"
+    PLN = "PLN"
+
+# Default categories enum
+class Category(StrEnum):
+    CAR = "Car"
+    FOOD = "Food"
+    TRANSPORT = "Transport"
+    ENTERTAINMENT = "Entertainment"
+    EATING_OUT = "Eating Out"
+    HEALTH = "Health"
+    CLOTHES = "Clothes"
+    TRAVEL = "Travel"
+    HOUSE = "House"
+    GIFTS = "Gifts"
+    COMMUNICATION = "Communication"
