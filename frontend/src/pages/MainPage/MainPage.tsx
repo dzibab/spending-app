68d32fa3d82c0ca7097675628edc71383ed4c592
Aliaksandra <a.piakhota@andersenlab.com>
2024-11-24 14:32:55 +0100

    add routing

---
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,13 @@
+export const MainPage = () => {
+  return (
+    <section>
+      <h1>TimeDate</h1>
+      <div>
+        diagramm
+      </div>
+      <div>
+        buttons
+      </div>
+    </section>
+  )
+}
---
+++ frontend/src/router/router.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from 'react-router-dom'
+import { Suspense } from 'react'
+import App from '../App'
+import { MainPage } from '../pages/MainPage/MainPage'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <Suspense fallback="">
+        <App />
+      </Suspense>
+    ),
+    children: [
+      {
+        index: true,
+        element: <MainPage />
+      }
+    ]
+  }
+])
+
