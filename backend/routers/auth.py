a4be98c3a082c8718d52f964c636fc79965bf317
DziBab <3169477@gmail.com>
2024-11-24 14:38:28 +0100

    Add auth pydantic models

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -3,19 +3,19 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
 from backend.db.session import get_db
-from backend.models.user import UserResponse
-from backend.utils.security import verify_password, create_access_token
 from backend.db.models import User as UserDB
+from backend.models.auth import LoginRequest, TokenResponse
+from backend.utils.security import verify_password, create_access_token
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 
-@router.post("/token", response_model=UserResponse)
-async def login_for_access_token(email: str, password: str, db: AsyncSession = Depends(get_db)):
-    result = await db.execute(select(UserDB).filter(UserDB.email == email))
+@router.post("/token", response_model=TokenResponse)
+async def login_for_access_token(login_request: LoginRequest, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(UserDB).filter(UserDB.email == login_request.email))
     user = result.scalars().first()
 
-    if not user or not verify_password(password, user.hashed_password):
+    if not user or not verify_password(login_request.password, user.hashed_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid credentials",
