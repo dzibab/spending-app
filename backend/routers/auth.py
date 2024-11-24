a9f53e4ba66a16e107487265777cee7da2da7346
DziBab <3169477@gmail.com>
2024-11-24 19:19:48 +0100

    Fix auth input model

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -1,21 +1,25 @@
 from fastapi import APIRouter, HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
 from backend.db.session import get_db
 from backend.db.models import User as UserDB
-from backend.models.auth import LoginRequest, TokenResponse
+
+from backend.models.auth import TokenResponse
 from backend.utils.security import verify_password, create_access_token
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 
 @router.post("/token", response_model=TokenResponse)
-async def login_for_access_token(login_request: LoginRequest, db: AsyncSession = Depends(get_db)):
-    result = await db.execute(select(UserDB).filter(UserDB.email == login_request.email))
+async def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)
+):
+    result = await db.execute(select(UserDB).filter(UserDB.email == form_data.username))
     user = result.scalars().first()
 
-    if not user or not verify_password(login_request.password, user.hashed_password):
+    if not user or not verify_password(form_data.password, user.hashed_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid credentials",
