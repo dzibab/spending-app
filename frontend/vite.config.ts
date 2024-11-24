b933bef470ccdc6de7bc2dc843a83329f314ffae
Aliaksandra <a.piakhota@andersenlab.com>
2024-11-24 15:19:06 +0100

    adjust config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, normalizePath } from 'vite'
 import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
-import svgr from 'vite-plugin-svgr'
+// import svgr from 'vite-plugin-svgr'
 
 // @ts-ignore
 import path from 'path'
@@ -13,6 +13,12 @@ export default defineConfig({
   plugins: [react(),
     viteTsconfigPaths(),
     basicSsl()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': 'http://localhost:8000'
+    }
+  },
   resolve: {
     // @ts-ignore
     alias: {
