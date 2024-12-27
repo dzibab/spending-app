dc503a5203b4e4cd5271457fbdd2db6a6d280925
DziBab <3169477@gmail.com>
2024-12-27 14:14:09 +0100

    Use container name as a contstant in start_backend.sh

--- start_backend.sh
+++ start_backend.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+
+CONTAINER_NAME="spending-app-backend-container"
+
 # Check if the Docker container is running
-if [ "$(docker ps -q -f name=spending-app-backend-container)" ]; then
+if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
   # Stop and remove the container if it is running
   ./stop_backend.sh
 fi
+
 # Create and run the Docker container in detached mode
 docker build -t spending-app-backend .
 
 # Create db file
 touch test.db
 
-docker run -d --name spending-app-backend-container \
+docker run -d --name $CONTAINER_NAME \
   -p 8000:8000 \
   -v $(pwd)/test.db:/app/test.db \
   spending-app-backend
